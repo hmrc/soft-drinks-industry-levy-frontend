1baf912d4c924172fb00ebe919fdd738e15f1dbc
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2017-11-17 16:13:12 +0000

    Implemented feedback from the PR

--- app/uk/gov/hmrc/softdrinksindustrylevy/connectors/SoftDrinksIndustryLevyConnector.scala
+++ app/uk/gov/hmrc/softdrinksindustrylevy/connectors/SoftDrinksIndustryLevyConnector.scala
@@ -18,14 +18,13 @@ package uk.gov.hmrc.softdrinksindustrylevy.connectors
 
 import javax.inject.Singleton
 
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.config.ServicesConfig
 import uk.gov.hmrc.softdrinksindustrylevy.config.WSHttp
 import uk.gov.hmrc.softdrinksindustrylevy.models.DesSubmissionResult
 
-import scala.concurrent.Future
-import uk.gov.hmrc.http.HeaderCarrier
-
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class SoftDrinksIndustryLevyConnector extends ServicesConfig {
