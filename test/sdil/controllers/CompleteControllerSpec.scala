fb732b58497e7685323bb7fb1a64dfa01b2c0615
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2017-12-28 11:52:58 +0000

    Deleted SDIL controller as it is no longer required Moved complete page into its own controller Deleted old routes and tests

---
+++ test/sdil/controllers/CompleteControllerSpec.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sdil.controllers
+
+import org.scalatest.BeforeAndAfterEach
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class CompleteControllerSpec extends ControllerSpec with BeforeAndAfterEach {
+
+  "Complete controller" should {
+    "return Status: OK for displaying complete page" in {
+      val result = testController.displayComplete().apply(FakeRequest())
+
+      status(result) mustBe OK
+      contentAsString(result) must include(messagesApi("sdil.complete.title"))
+    }
+  }
+
+  val testController: CompleteController = wire[CompleteController]
+
+}
