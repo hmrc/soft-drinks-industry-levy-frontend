70d9d1f3d1386cee59bcfcee6711c655211e3010
Richard Dennehy <richard.dennehy@outlook.com>
2018-01-12 15:23:08 +0000

    Sdil 972 adding reset endpoint (#97)
    * endpoint for stub teardown
    
    * modified endpoint route
    
    * add configuration to wiring to allow overriding the application router

---
+++ app/sdil/connectors/TestConnector.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sdil.connectors
+
+import play.api.{Configuration, Environment}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.play.config.ServicesConfig
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class TestConnector(http: HttpClient,
+                    environment: Environment,
+                    val runModeConfiguration: Configuration)
+                   (implicit ec: ExecutionContext) extends ServicesConfig {
+
+  lazy val testUrl: String = baseUrl("soft-drinks-industry-levy")
+
+  override protected def mode = environment.mode
+
+  def resetStore(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] = {
+    http.GET[HttpResponse](s"$testUrl/test-only/reset")
+  }
+
+}
---
+++ app/sdil/controllers/test/TestingController.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sdil.controllers.test
+
+import play.api.mvc.{Action, AnyContent}
+import sdil.connectors.TestConnector
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+
+
+class TestingController (testConnector: TestConnector) extends FrontendController {
+
+  def resetStore: Action[AnyContent] = Action.async {
+    implicit request =>
+      testConnector.resetStore map {
+        x => Status(x.status)
+      }
+  }
+
+}
