94b19298e6da172762620f356f4576d62c1201cb
Richard Dennehy <richard.dennehy@digital.hmrc.gov.uk>
2018-02-02 11:25:59 +0000

    disable show/hide js on ie8 and ie9 to make the site functional

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,11 +5,16 @@
 
 $(document).ready(function () {
     // Turn off jQuery animation
-    jQuery.fx.off = true
+    jQuery.fx.off = true;
 
     // Where .multiple-choice uses the data-target attribute
     // to toggle hidden content
-    var showHideContent = new GOVUK.ShowHideContent()
-    showHideContent.init()
+    var showHideContent = new GOVUK.ShowHideContent();
 
-})
+    function isIE(version) { new GOVUK.IsIE(version) }
+
+    //disable show/hide js when running IE 8 or IE 9, as the show functionality won't work
+    if(!isIE(8) && !isIE(9)) {
+        showHideContent.init()
+    }
+});
