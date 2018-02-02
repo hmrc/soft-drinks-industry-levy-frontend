ade5e064c1c24dbc7292fce6eba2009d3c09456f
Richard Dennehy <richard.dennehy@outlook.com>
2018-02-02 14:07:26 +0000

    trying to fix ie again (#136)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,10 +11,5 @@ $(document).ready(function () {
     // to toggle hidden content
     var showHideContent = new GOVUK.ShowHideContent();
 
-    function isIE(version) { new GOVUK.IsIE(version) }
-
-    //disable show/hide js when running IE 8 or IE 9, as the show functionality won't work
-    if(!isIE(8) && !isIE(9)) {
-        showHideContent.init()
-    }
+    showHideContent.init()
 });
