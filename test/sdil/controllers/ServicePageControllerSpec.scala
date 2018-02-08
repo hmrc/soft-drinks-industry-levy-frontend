a6e527bcfedbeace7454ea6a90061515b3593cf5
Adam Dye <adam.dye@digital.hmrc.gov.uk>
2018-02-08 15:16:49 +0000

    Completed unit tests for service page.

--- test/sdil/controllers/ServicePageControllerSpec.scala
+++ test/sdil/controllers/ServicePageControllerSpec.scala
@@ -1,17 +1,187 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package sdil.controllers
 
-import java.time.LocalDateTime
+import java.time.LocalDate
 
-import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.{any, eq => matching}
 import org.mockito.Mockito.when
+import org.scalatest.BeforeAndAfterAll
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{contentAsString, status}
-import sdil.models.SubmissionData
+import play.api.test.Helpers._
+import sdil.actions.RegisteredAction
+import sdil.models.backend.{Contact, Site, UkAddress}
+import sdil.models.retrieved.{RetrievedActivity, RetrievedSubscription}
+import uk.gov.hmrc.auth.core.{Enrolment, EnrolmentIdentifier, Enrolments}
+import uk.gov.hmrc.auth.core.retrieve.Retrievals._
 
 import scala.concurrent.Future
 
-//class ServicePageControllerSpec {
-//  "Service page controller" should {
-//    "return Status: OK for displaying service page" in {
-//
-//}
+
+class ServicePageControllerSpec extends ControllerSpec with BeforeAndAfterAll {
+
+  "Service page controller" should {
+    "return Status: OK for displaying service page" in {
+      val sdilEnrolment = EnrolmentIdentifier("EtmpRegistrationNumber", "XZSDIL000100107")
+      when(mockAuthConnector.authorise[Enrolments](any(), matching(allEnrolments))(any(), any())).thenReturn {
+        Future.successful(Enrolments(Set(Enrolment("HMRC-OBTDS-ORG", Seq(sdilEnrolment), "Active"))))
+      }
+
+      when(mockSdilConnector.retrieveSubscription(matching("XZSDIL000100107"))(any())).thenReturn {
+        Future.successful(validRetrievedSubscription)
+      }
+
+      val request = FakeRequest("GET", "/home")
+      val result = testController.show.apply(request)
+
+      status(result) mustBe OK
+      contentAsString(result) must include(messagesApi("sdil.service-page.title"))
+      contentAsString(result) mustNot include(messagesApi("sdil.service-page.p2.voluntary-only"))
+    }
+
+    "return Status: OK for displaying service page and voluntary information for voluntary registration" in {
+      val sdilEnrolment = EnrolmentIdentifier("EtmpRegistrationNumber", "XZSDIL000100107")
+      when(mockAuthConnector.authorise[Enrolments](any(), matching(allEnrolments))(any(), any())).thenReturn {
+        Future.successful(Enrolments(Set(Enrolment("HMRC-OBTDS-ORG", Seq(sdilEnrolment), "Active"))))
+      }
+
+      when(mockSdilConnector.retrieveSubscription(matching("XZSDIL000100107"))(any())).thenReturn {
+        Future.successful(validVoluntaryRetrievedSubscription)
+      }
+      val request = FakeRequest("GET", "/home")
+      val result = testController.show.apply(request)
+
+      status(result) mustBe OK
+      contentAsString(result) must include(messagesApi("sdil.service-page.p2.voluntary-only"))
+    }
+
+    "return Status: OK for displaying service page and packaging sites for users who registered them" in {
+      val sdilEnrolment = EnrolmentIdentifier("EtmpRegistrationNumber", "XZSDIL000100107")
+      when(mockAuthConnector.authorise[Enrolments](any(), matching(allEnrolments))(any(), any())).thenReturn {
+        Future.successful(Enrolments(Set(Enrolment("HMRC-OBTDS-ORG", Seq(sdilEnrolment), "Active"))))
+      }
+
+      when(mockSdilConnector.retrieveSubscription(matching("XZSDIL000100107"))(any())).thenReturn {
+        Future.successful(validPackagingRetrievedSubscription)
+      }
+      val request = FakeRequest("GET", "/home")
+      val result = testController.show.apply(request)
+
+      status(result) mustBe OK
+      contentAsString(result) must include(messagesApi("sdil.service-page.packaging-address.subtitle"))
+    }
+
+    "return Status: OK for displaying service page and warehouses for users who registered them" in {
+      val sdilEnrolment = EnrolmentIdentifier("EtmpRegistrationNumber", "XZSDIL000100107")
+      when(mockAuthConnector.authorise[Enrolments](any(), matching(allEnrolments))(any(), any())).thenReturn {
+        Future.successful(Enrolments(Set(Enrolment("HMRC-OBTDS-ORG", Seq(sdilEnrolment), "Active"))))
+      }
+
+      when(mockSdilConnector.retrieveSubscription(matching("XZSDIL000100107"))(any())).thenReturn {
+        Future.successful(validWarehouseRetrievedSubscription)
+      }
+      val request = FakeRequest("GET", "/home")
+      val result = testController.show.apply(request)
+
+      status(result) mustBe OK
+      contentAsString(result) must include(messagesApi("sdil.service-page.warehouse-address.subtitle"))
+    }
+
+    "return Status: NOT_FOUND for displaying service page with no enrolment" in {
+      val sdilEnrolment = EnrolmentIdentifier("EtmpRegistrationNumber", "XZSDIL000100107")
+      when(mockAuthConnector.authorise[Enrolments](any(), matching(allEnrolments))(any(), any())).thenReturn {
+        Future.successful(Enrolments(Set(Enrolment("HMRC-OBTDS-ORG", Seq(sdilEnrolment), "Active"))))
+      }
+
+      when(mockSdilConnector.retrieveSubscription(matching("XZSDIL000100107"))(any())).thenReturn {
+        Future.successful(None)
+      }
+      val request = FakeRequest("GET", "/home")
+      val result = testController.show.apply(request)
+
+      status(result) mustBe NOT_FOUND
+    }
+  }
+
+  val validRetrievedSubscription = Some(RetrievedSubscription(
+    "111222333",
+    "Cliff's Limonard",
+    Some("3"),
+    UkAddress(Seq("1", "The Road"), "AA11 1AA"),
+    RetrievedActivity(false, false, true, false, false),
+    LocalDate.of(2018, 4, 6),
+    List(Site(UkAddress(Seq("1 Production Site St", "Production Site Town"), "AA11 1AA"))),
+    List(Site(UkAddress(Seq("1 Warehouse Site St", "Warehouse Site Town"), "AA11 1AA"))),
+    Contact(
+      Some("A person"),
+      Some("A position"),
+      "1234",
+      "aa@bb.cc")
+  ))
+
+  val validVoluntaryRetrievedSubscription = Some(RetrievedSubscription(
+    "111222333",
+    "Cliff's Limonard",
+    Some("3"),
+    UkAddress(Seq("1", "The Road"), "AA11 1AA"),
+    RetrievedActivity(false, true, true, false, true),
+    LocalDate.of(2018, 4, 6),
+    List(Site(UkAddress(Seq("1 Production Site St", "Production Site Town"), "AA11 1AA"))),
+    List(Site(UkAddress(Seq("1 Warehouse Site St", "Warehouse Site Town"), "AA11 1AA"))),
+    Contact(
+      Some("A person"),
+      Some("A position"),
+      "1234",
+      "aa@bb.cc")
+  ))
+
+  val validPackagingRetrievedSubscription = Some(RetrievedSubscription(
+    "111222333",
+    "Cliff's Limonard",
+    Some("3"),
+    UkAddress(Seq("1", "The Road"), "AA11 1AA"),
+    RetrievedActivity(false, true, true, false, true),
+    LocalDate.of(2018, 4, 6),
+    List(Site(UkAddress(Seq("", ""), ""))),
+    List(Site(UkAddress(Seq("1 Warehouse Site St", "Warehouse Site Town"), "AA11 1AA"))),
+    Contact(
+      Some("A person"),
+      Some("A position"),
+      "1234",
+      "aa@bb.cc")
+  ))
+
+  val validWarehouseRetrievedSubscription = Some(RetrievedSubscription(
+    "111222333",
+    "Cliff's Limonard",
+    Some("3"),
+    UkAddress(Seq("1", "The Road"), "AA11 1AA"),
+    RetrievedActivity(false, true, true, false, true),
+    LocalDate.of(2018, 4, 6),
+    List(Site(UkAddress(Seq("1 Production Site St", "Production Site Town"), "AA11 1AA"))),
+    List(Site(UkAddress(Seq("", ""), ""))),
+    Contact(
+      Some("A person"),
+      Some("A position"),
+      "1234",
+      "aa@bb.cc")
+  ))
+
+  lazy val registeredAction: RegisteredAction = wire[RegisteredAction]
+  lazy val testController: ServicePageController = wire[ServicePageController]
+
+}
