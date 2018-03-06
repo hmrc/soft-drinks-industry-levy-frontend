1d7b8cdb3c463ef0ef8eb0fb21f39954389fa104
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2018-03-06 12:05:14 +0000

    Added ids to allow page events to be tracked Updated js file to include new events for the page views

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,8 +15,21 @@ $(document).ready(function () {
 });
 
 window.onload = function () {
-    if(document.getElementsByClassName("flash error-summary error-summary--show").length > 0) {
+    if (document.getElementsByClassName("flash error-summary error-summary--show").length > 0) {
         ga('send', 'event', 'validationError', 'error', window.location.pathname);
+    }
+
+    if (window.location.href.indexOf("start") > -1) {
+        if (document.getElementById("registration-status").textContent.indexOf("agent") > -1) {
+            ga('send', 'event', 'agents', 'visited', 'agentsPageVisited');
+        } else if (document.getElementById("registration-status").textContent.indexOf("assistant") > -1) {
+            ga('send', 'event', 'assistants', 'visited', 'assistantsPageVisited');
+        } else if (document.getElementById("registration-status").textContent.indexOf("already registered") > -1) {
+            ga('send', 'event', 'alreadyRegistered', 'visited', 'alreadyRegisteredPageVisited');
+        }
+
+    } else if (window.location.href.indexOf("verify") > -1 && document.getElementById("registration-pending-title")) {
+        ga('send', 'event', 'pending', 'visited', 'pendingPageVisited');
     }
 
 };
