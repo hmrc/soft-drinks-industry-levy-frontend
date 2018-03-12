19833d31f8ccbfc42887e6975e1e102f3772b72f
Nick Karaolis <nick.karaolis@digital.hmrc.gov.uk>
2018-03-12 13:54:01 +0000

    Removed checking for arrow keys

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,9 +15,6 @@ $(document).ready(function () {
 
     $('input.volume').keyup(function(event) {
 
-        // skip for arrow keys
-        if(event.which >= 37 && event.which <= 40) return;
-
         // format number
         $(this).val(function(index, value) {
             return value
