5c953713b47fd53b6547b126092d48d51494342a
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2018-09-26 15:05:58 +0100

    fix litreage order used in difference check page

--- app/sdil/controllers/package.scala
+++ app/sdil/controllers/package.scala
@@ -30,8 +30,8 @@ package object controllers {
   val returnLiterageList = List(
     "own-brands-packaged-at-own-sites",
     "packaged-as-a-contract-packer",
-    "brought-into-uk-from-small-producers",
     "brought-into-uk",
+    "brought-into-uk-from-small-producers",
     "exemptions-for-small-producers",
     "claim-credits-for-exports",
     "claim-credits-for-lost-damaged"
