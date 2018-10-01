463ff8c4518d71f955a5382aae2d862107bd38bd
adamdye-hmrc <adam.dye@digital.hmrc.gov.uk>
2018-10-01 18:05:43 +0100

    Used extraMessages instead of passing the period around.

--- app/sdil/controllers/SdilWMController.scala
+++ app/sdil/controllers/SdilWMController.scala
@@ -81,8 +81,7 @@ trait SdilWMController extends WebMonadController
   def cya(
     key: String,
     mainContent: Html,
-    editRoutes: PartialFunction[String, WebMonad[Unit]] = Map.empty,
-    period: Option[ReturnPeriod] = None
+    editRoutes: PartialFunction[String, WebMonad[Unit]] = Map.empty
   )(implicit extraMessages: ExtraMessages, showBackLink: ShowBackLink): WebMonad[Unit] = {
 
     def getMapping(path: List[String]) = {
@@ -93,7 +92,7 @@ trait SdilWMController extends WebMonadController
       path    <- getPath
       r  <- formPage(key)(getMapping(path), None) { (path, form, r) =>
         implicit val request: Request[AnyContent] = r
-        uniform.cya(key, form, path, mainContent, period)
+        uniform.cya(key, form, path, mainContent)
       }.flatMap {
         case "DONE" => ().pure[WebMonad]
         case x if editRoutes.isDefinedAt(x) => clear(key) >> editRoutes.apply(x)
@@ -108,7 +107,6 @@ trait SdilWMController extends WebMonadController
     broughtForward: BigDecimal,
     subscription: RetrievedSubscription,
     variation: Option[ReturnsVariation] = None,
-    period: Option[ReturnPeriod] = None,
     alternativeRoutes: PartialFunction[String, WebMonad[Unit]] = Map.empty,
     originalReturn: Option[SdilReturn] = None
   )(implicit extraMessages: ExtraMessages, showBackLink: ShowBackLink): WebMonad[Unit] = {
@@ -129,17 +127,13 @@ trait SdilWMController extends WebMonadController
       subscription,
       originalReturn)
 
-    cya(
-      key,
-      inner,
-      {
-        case "exemptions-for-small-producers" =>
-          write[Boolean]("_editSmallProducers", true) >>
-            clear(key) >>
-            resultToWebMonad[Unit](Redirect("exemptions-for-small-producers"))
-      }, 
-      period
-    )
+    cya(key, inner,
+        {
+          case "exemptions-for-small-producers" =>
+            write[Boolean]("_editSmallProducers", true) >>
+              clear(key) >>
+              resultToWebMonad[Unit](Redirect("exemptions-for-small-producers"))
+        })
   }
 
   def checkReturnChanges(key: String, variation: ReturnVariationData) = {
