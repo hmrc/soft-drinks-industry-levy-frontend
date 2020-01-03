e2e7bc30e104fc70f27aeb9c1cfc3b94725008e0
Alex Newby <41003702+alex-newby-hmrc@users.noreply.github.com>
2020-01-03 08:06:22 +0000

    Minor test refactoring

--- test/sdil/controllers/PaymentControllerSpec.scala
+++ test/sdil/controllers/PaymentControllerSpec.scala
@@ -34,10 +34,11 @@ class PaymentControllerSpec extends ControllerSpec {
     lazy val testController = wire[PaymentController]
 
     val testRedirectUrl = "/test-url"
-    val testPayApiRequest = SpjRequestBtaSdil("XKSDIL000000033", 0L, testConfig.sdilHomePage, testConfig.sdilHomePage)
     val testSdilEnrolment = EnrolmentIdentifier("EtmpRegistrationNumber", "XKSDIL000000033")
 
     "contact pay-api and get a redirect url" in {
+      val testPayApiRequest = SpjRequestBtaSdil("XKSDIL000000033", 0L, testConfig.sdilHomePage, testConfig.sdilHomePage)
+
       when(mockAuthConnector.authorise[Enrolments](any(), matching(allEnrolments))(any(), any())).thenReturn {
         Future.successful(Enrolments(Set(Enrolment("HMRC-OBTDS-ORG", Seq(testSdilEnrolment), "Active"))))
       }
@@ -56,6 +57,10 @@ class PaymentControllerSpec extends ControllerSpec {
     "convert a negative balance into a positive amount in pence for the user to pay" in {
       val testPayApiRequestWithAmount =
         SpjRequestBtaSdil("XKSDIL000000033", 1000L, testConfig.sdilHomePage, testConfig.sdilHomePage)
+
+      when(mockAuthConnector.authorise[Enrolments](any(), matching(allEnrolments))(any(), any())).thenReturn {
+        Future.successful(Enrolments(Set(Enrolment("HMRC-OBTDS-ORG", Seq(testSdilEnrolment), "Active"))))
+      }
 
       when(mockSdilConnector.balance(any(), any())(any())).thenReturn(Future.successful(BigDecimal(-10)))
 
