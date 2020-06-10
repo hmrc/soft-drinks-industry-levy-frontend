bd0b9c99a04aac41f936cb22e8df404ede28c44a
John Revill <johnrevill@users.noreply.github.com>
2020-06-10 14:25:49 +0100

    [OPS-4318] Added link, controller and connector to start the direct debit journey

--- app/sdil/connectors/DirectDebitBackendConnector.scala
+++ app/sdil/connectors/DirectDebitBackendConnector.scala
@@ -14,41 +14,35 @@
  * limitations under the License.
  */
 
-//
-//package sdil.connectors
-//
-//import play.api.libs.json.{Format, Json}
-//import uk.gov.hmrc.http.HeaderCarrier
-//import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
-//import uk.gov.hmrc.play.bootstrap.http.HttpClient
-//
-//import scala.concurrent.{ExecutionContext, Future}
-//
-//class DirectDebitBackendConnector(http: HttpClient, config: ServicesConfig) {
-//
-//  lazy val directDebitBaseUrl: String = s"${config.baseUrl("pay-api")}/pay-api"
-//
-//  def getSdilPayLink(
-//    spjRequest: SpjRequestBtaSdil)(implicit headerCarrier: HeaderCarrier, ec: ExecutionContext): Future[NextUrl] =
-//    http.POST[SpjRequestBtaSdil, NextUrl](
-//      s"$payApiBaseUrl/bta/sdil/journey/start",
-//      spjRequest,
-//      Seq("X-Session-Id" -> headerCarrier.sessionId.get.value))
-//}
+package sdil.connectors
 
-//final case class SpjRequestBtaSdil(
-//  reference: String,
-//  amountInPence: Long,
-//  returnUrl: String,
-//  backUrl: String
-//)
-//
-//object SpjRequestBtaSdil {
-//  implicit val format: Format[SpjRequestBtaSdil] = Json.format[SpjRequestBtaSdil]
-//}
-//
-//final case class NextUrl(nextUrl: String)
-//
-//object NextUrl {
-//  implicit val nextUrlFormat: Format[NextUrl] = Json.format[NextUrl]
-//}
+import play.api.libs.json.{Format, Json}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class DirectDebitBackendConnector(http: HttpClient, config: ServicesConfig)(implicit ec: ExecutionContext) {
+
+  lazy val directDebitBaseUrl: String = s"${config.baseUrl("direct-debit-backend")}/direct-debit-backend"
+
+  def getSdilDirectDebitLink(request: StartSdilReturnFromSdilFrontend)(
+    implicit headerCarrier: HeaderCarrier): Future[DirectDebitNextUrl] =
+    http.POST[StartSdilReturnFromSdilFrontend, DirectDebitNextUrl](
+      s"$directDebitBaseUrl/bta/zsdl/journey/start",
+      request
+    )
+}
+
+final case class StartSdilReturnFromSdilFrontend(returnUrl: String, backUrl: String)
+
+object StartSdilReturnFromSdilFrontend {
+  implicit val format: Format[StartSdilReturnFromSdilFrontend] = Json.format[StartSdilReturnFromSdilFrontend]
+}
+
+final case class DirectDebitNextUrl(nextUrl: String)
+
+object DirectDebitNextUrl {
+  implicit val nextUrlFormat: Format[DirectDebitNextUrl] = Json.format[DirectDebitNextUrl]
+}
