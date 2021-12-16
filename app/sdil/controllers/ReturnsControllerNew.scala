3833aeb52610eca0b73729d3208778d8761aabc1
Adam Dye <31273792+adam-dye@users.noreply.github.com>
2021-12-16 01:12:37 +0000

    reformatted ReturnsControllerNew.

--- app/sdil/controllers/ReturnsControllerNew.scala
+++ app/sdil/controllers/ReturnsControllerNew.scala
@@ -112,8 +112,6 @@ object ReturnsControllerNew {
     submitReturn: SdilReturn => Future[Unit]
   ) = {
 
-    case object ChangeRegistration
-
     val costLower = BigDecimal("0.18")
     val costHigher = BigDecimal("0.24")
 
@@ -141,31 +139,32 @@ object ReturnsControllerNew {
     }
 
     for {
-      ownBrands <- askEmptyOption[(Long, Long)](
-                    "own-brands-packaged-at-own-sites",
-                    default = default.map { _.packLarge }
-                  ) emptyUnless !subscription.activity.smallProducer
-      contractPacked <- askEmptyOption[(Long, Long)](
-                         "packaged-as-a-contract-packer"
-                       ) emptyUnless !subscription.activity.smallProducer
-      smallProds <- askListSimple[SmallProducer](
-                     "small-producer-details",
-                     "add-small-producer",
-                     validation = Rule.nonEmpty[List[SmallProducer]],
-                     default = default.map { _.packSmall }
-                   ) emptyUnless ask[Boolean]("exemptions-for-small-producers", default = default.map {
-                     _.packSmall.nonEmpty
-                   })
-      imports <- askEmptyOption[(Long, Long)]("brought-into-uk", default.map { _.importLarge })
-      importsSmall <- askEmptyOption[(Long, Long)]("brought-into-uk-from-small-producers", default.map {
-                       _.importSmall
-                     })
-      exportCredits <- askEmptyOption[(Long, Long)]("claim-credits-for-exports", default.map { _.export })
-      wastage       <- askEmptyOption[(Long, Long)]("claim-credits-for-lost-damaged", default.map { _.wastage })
-      sdilReturn = SdilReturn(ownBrands, contractPacked, smallProds, imports, importsSmall, exportCredits, wastage)
-      isNewImporter = !sdilReturn.totalImported.isEmpty && !subscription.activity.importer
-      isNewPacker = !sdilReturn.totalPacked.isEmpty && !subscription.activity.contractPacker
-      //      _               <- tell("return-change-registration", ChangeRegistration) when isNewImporter || isNewPacker
+      ownBrands       <- askEmptyOption[(Long, Long)](
+                          "own-brands-packaged-at-own-sites",
+                          default = default.map { _.packLarge }
+                        ) emptyUnless !subscription.activity.smallProducer
+      contractPacked  <- askEmptyOption[(Long, Long)](
+                          "packaged-as-a-contract-packer"
+                        ) emptyUnless !subscription.activity.smallProducer
+      smallProds      <- askListSimple[SmallProducer](
+                          "small-producer-details",
+                          "add-small-producer",
+                          validation = Rule.nonEmpty[List[SmallProducer]],
+                          default = default.map { _.packSmall }
+                        ) emptyUnless ask[Boolean]("exemptions-for-small-producers", default = default.map {
+                          _.packSmall.nonEmpty
+                        })
+      imports         <- askEmptyOption[(Long, Long)]("brought-into-uk", default.map { _.importLarge })
+      importsSmall    <- askEmptyOption[(Long, Long)]("brought-into-uk-from-small-producers", default.map {
+                           _.importSmall
+                         })
+      exportCredits   <- askEmptyOption[(Long, Long)]("claim-credits-for-exports", default.map { _.export })
+      wastage         <- askEmptyOption[(Long, Long)]("claim-credits-for-lost-damaged", default.map { _.wastage })
+      sdilReturn      = SdilReturn(ownBrands, contractPacked, smallProds, imports, importsSmall, exportCredits, wastage)
+      isNewImporter   = !sdilReturn.totalImported.isEmpty && !subscription.activity.importer
+      isNewPacker     = !sdilReturn.totalPacked.isEmpty && !subscription.activity.contractPacker
+      inner           = uniform.fragments.return_variation_continue(isNewImporter, isNewPacker)(_: Messages)
+      _               <- tell("return-change-registration", inner) when isNewImporter || isNewPacker
       newPackingSites <- (
                           for {
                             firstPackingSite <- ask[Boolean]("pack-at-business-address-in-return") flatMap {
@@ -180,27 +179,27 @@ object ReturnsControllerNew {
                                            )
                           } yield packingSites
                         ) when isNewPacker && subscription.productionSites.isEmpty
-      newWarehouses <- (for {
-                        addWarehouses <- ask[Boolean]("ask-secondary-warehouses-in-return")
-                        warehouses <- askListSimple[Site](
-                                       "secondary-warehouse-details",
-                                       validation = Rule.nonEmpty[List[Site]]
-                                     ) emptyUnless addWarehouses
-                      } yield warehouses) when isNewImporter && subscription.warehouseSites.isEmpty
-      variation = ReturnsVariation(
-        orgName = subscription.orgName,
-        ppobAddress = subscription.address,
-        importer = (isNewImporter, (sdilReturn.totalImported).combineN(4)),
-        packer = (isNewPacker, (sdilReturn.totalPacked).combineN(4)),
-        warehouses = newWarehouses.getOrElse(List.empty[Site]),
-        packingSites = newPackingSites.getOrElse(List.empty[Site]),
-        phoneNumber = subscription.contact.phoneNumber,
-        email = subscription.contact.email,
-        taxEstimation = taxEstimation(sdilReturn)
-      )
-      data = returnAmount(sdilReturn, isSmallProd)
+      newWarehouses  <- (for {
+                         addWarehouses <- ask[Boolean]("ask-secondary-warehouses-in-return")
+                         warehouses <- askListSimple[Site](
+                                        "secondary-warehouse-details",
+                                        validation = Rule.nonEmpty[List[Site]]
+                                      ) emptyUnless addWarehouses
+                       } yield warehouses) when isNewImporter && subscription.warehouseSites.isEmpty
+      variation      = ReturnsVariation(
+                         orgName = subscription.orgName,
+                         ppobAddress = subscription.address,
+                         importer = (isNewImporter, (sdilReturn.totalImported).combineN(4)),
+                         packer = (isNewPacker, (sdilReturn.totalPacked).combineN(4)),
+                         warehouses = newWarehouses.getOrElse(List.empty[Site]),
+                         packingSites = newPackingSites.getOrElse(List.empty[Site]),
+                         phoneNumber = subscription.contact.phoneNumber,
+                         email = subscription.contact.email,
+                         taxEstimation = taxEstimation(sdilReturn)
+                       )
+      data     = returnAmount(sdilReturn, isSmallProd)
       subtotal = calculateSubtotal(data)
-      total = subtotal - broughtForward
+      total    = subtotal - broughtForward
       _ <- tell(
             "check-your-answers",
             uniform.fragments.returnsCYA(
@@ -238,7 +237,11 @@ object ReturnsControllerNew {
                   f"£$total%,.2f"
 
               val prettyPeriod =
-                msg(s"period.check-your-answers", period.start.format("MMMM"), period.end.format("MMMM yyyy"))
+                msg(
+                  s"period.check-your-answers",
+                  period.start.format("MMMM"),
+                  period.end.format("MMMM yyyy")
+                )
 
               val getTotal =
                 if (total <= 0)
