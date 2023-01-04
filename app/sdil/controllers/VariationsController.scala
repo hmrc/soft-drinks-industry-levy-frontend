06965b0d31e8a13f72daab166888dced37aef4cd
Graeme Stirling <graemejstirling@gmail.com>
2023-01-04 14:20:17 +0000

    update t0 2023

--- app/sdil/controllers/VariationsController.scala
+++ app/sdil/controllers/VariationsController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -311,7 +311,6 @@ class VariationsController @Inject()(
     val originalBusinessDetails = List(
       variation.original.address.postCode
     ) ++ variation.original.address.lines
-
 
     if ((updatedContactDetails equals (originalContactDetails)) && (updatedBusinessDetails equals originalBusinessDetails) || (updatedContactDetails != (originalContactDetails)) && (updatedBusinessDetails != originalBusinessDetails)) {
       List("contact-details", "business-address")
